ddca1a1a43267f65f2bff36c0c4f09764f328201
NirajSahu0412 <nirajsahu0412@gmail.com>
2026-05-29 23:54:43 +0530

    SQL APP

---
+++ SimpleSQLApp/Home.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SimpleSQLApp.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>ID:</td>
+                    <td>
+                        <asp:TextBox ID="idBox" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Name:</td>
+                    <td>
+                        <asp:TextBox ID="nameBox" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>City:</td>
+                    <td>
+                        <asp:TextBox ID="cityBox" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="Button1_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="Button2_Click" />
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="Button3_Click" />
+                        <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SimpleSQLApp/Home.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Security;
+
+namespace SimpleSQLApp
+{
+    public partial class Home : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Project\NET-Project\SimpleSQLApp\App_Data\User.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open) 
+            {
+                con.Close();
+            }
+            con.Open();
+            displayData();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into [user] values ('"+ nameBox.Text +"','"+ cityBox.Text +"')";
+            cmd.ExecuteNonQuery();
+
+            nameBox.Text = string.Empty;
+            cityBox.Text = string.Empty;
+
+            displayData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete FROM [user] where id="+ Convert.ToInt32(idBox.Text) +"";
+            cmd.ExecuteNonQuery();
+
+            idBox.Text= string.Empty;
+            displayData();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update [user] set name='"+nameBox.Text+"', city='"+cityBox.Text+"' where id="+ Convert.ToInt32(idBox.Text) +"";
+            cmd.ExecuteNonQuery();
+
+            idBox.Text = string.Empty;
+            nameBox.Text = string.Empty;
+            cityBox.Text = string.Empty;
+
+            displayData();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            displayData();
+        }
+
+        protected void displayData()
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [user]";
+            cmd.ExecuteNonQuery();
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
